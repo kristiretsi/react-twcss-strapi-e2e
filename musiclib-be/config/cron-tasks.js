90653732f50d6e715c1242faff65f309a82420af
kristiretsi <kristireci01@gmail.com>
2026-06-05 11:25:12 +0300

    bug

--- musiclib-be/config/cron-tasks.js
+++ musiclib-be/config/cron-tasks.js
@@ -1,10 +1,10 @@
-module.exports = {
-    syncArtists: {
-        task: async ({ strapi }) => {
-            await strapi.service("api::artist.artist").syncTopArtists();
-        },
-        options: {
-            rule: "/10 * * * *",
-        },
-    },
-};
+// module.exports = {
+//     syncArtists: {
+//         task: async ({ strapi }) => {
+//             await strapi.service("api::artist.artist").syncTopArtists();
+//         },
+//         options: {
+//             rule: "/10 * * * *",
+//         },
+//     },
+// };
--- musiclib-be/src/index.js
+++ musiclib-be/src/index.js
@@ -1,18 +1,43 @@
-module.exports = {
-  register({ strapi }) {
-    strapi.cron.add({
-      "*/30 * * * * *": async () => {
-        // "* * * * *": async () => {
-        // "/10 * * * *": async () => {
-        await strapi
-          .service("api::artist.artist")
-          .syncTopArtists();
+// module.exports = {
+//   // register({ strapi }) {
+//   //   strapi.cron.add({
+//   //     "*/30 * * * * *": async () => {
+//   //       // "* * * * *": async () => {
+//   //       // "/10 * * * *": async () => {
+//   //       await strapi
+//   //         .service("api::artist.artist")
+//   //         .syncTopArtists();
 
+//   //     },
+//   //   });
+//   // },
+//   register({ strapi }) {
+//     console.log("🟢 REGISTER CALLED");
+
+//     strapi.cron.add({
+//       "*/30 * * * * *": async () => {
+//         console.log("🔥 CRON TRIGGERED", new Date().toISOString());
+
+//         await strapi.service("api::artist.artist").syncTopArtists();
+//       },
+//     });
+//   },
+//   async bootstrap({ strapi }) {
+//     //TEMP: run once then remove
+//     //await require('../scripts/delete-artists')({ strapi });
+//   },
+// };
+
+module.exports = {
+  async bootstrap({ strapi }) {
+    console.log("🟢 BOOTSTRAP CALLED");
+
+    strapi.cron.add({
+      "*/50 * * * * *": async () => {
+        console.log("🔥 CRON TRIGGERED", new Date().toISOString());
+
+        await strapi.service("api::artist.artist").syncTopArtists();
       },
     });
-  },
-  async bootstrap({ strapi }) {
-    //TEMP: run once then remove
-    //await require('../scripts/delete-artists')({ strapi });
   },
 };
